bf82958f5cb1f35c0a4406830c97eddc33d627d1
Jonathan Warner <warnerjon12@gmail.com>
2025-07-01 13:20:05 -0600

    Fixed get_window_fields to account for half cell offset correctly

--- mw_fdtd/plotting.py
+++ mw_fdtd/plotting.py
@@ -15,12 +15,21 @@ def plot_field(ax: plt.Axes, field: np.ndarray, vmin=-100, vmax=0, cmap="terrain
 
     return pcm
 
-def get_window_fields(field_val, field_loc, order=4):
+def get_window_fields(field_val, field_loc, order=4, field="ez"):
     """
     Interpolates field_val at the field_loc points. Useful for comparing the moving window fields with a larger
     static grid. 
     """
 
+    x_loc, z_loc = field_loc[0], field_loc[2]
+    if field.lower() == "ez":
+        z_loc -= 0.5
+    elif field.lower() == "ex":
+        x_loc -= 0.5
+    elif field.lower() in ("h", "hy"):
+        z_loc -= 0.5
+        x_loc -= 0.5
+
     return ndimage.map_coordinates(
-        field_val, np.array([field_loc[0], field_loc[2]]), mode="constant", cval=0, order=order
+        field_val, np.array([x_loc, z_loc]), mode="constant", cval=0, order=order
     )
